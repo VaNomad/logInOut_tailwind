8a1dad1ef0520e4dac6f935f2b23c41ab3670c7e
VaNomad <sjur.hassel@gmail.com>
2023-04-13 18:45:35 +0100

    cleaned code

--- js/utils.mjs
+++ js/utils.mjs
@@ -2,18 +2,6 @@
 // Logo Hover ————————————————————————————————————————————————————————————————————————— >
 const logo = document.querySelector("#logo");
 
-
-
-// logo.addEventListener("mouseover", () => {
-//   logo.src = "/assets/vectors/sjurio-logo_pink_reverse.png"
-//   logo.to
-// })
-
-// logo.addEventListener("mouseout", () => {
-//   logo.src = "/assets/vectors/sjurio-logo-black copy.png";
-
-// })
-
 logo.addEventListener("mouseover", () => {
   logo.src = "/assets/vectors/sjurio-logo_pink_reverse.png"
 })
@@ -22,16 +10,6 @@ logo.addEventListener("mouseout", () => {
   logo.src = "/assets/vectors/sjurio-logo-black copy.png";
 })
 
-// Global eventlistener
-// addGlobalEventlistener("click", "div", e => {
-//   console.log("hi");
-// })
-
-// function addGlobalEventlistener(type, selector, callback) {
-//   document.addEventListener(type, e => {
-//     if (e.target.matches(selector)) callback(e)
-//   })
-// }
 
 // Active Nav Link ————————————————————————————————————————————————————————————————————————— >
 const activePage = window.location.pathname;
