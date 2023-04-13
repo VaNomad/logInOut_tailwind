803958aa9db39685ee39677adeb9524368c30040
VaNomad <sjur.hassel@gmail.com>
2023-04-13 19:39:48 +0100

    Adds router + xtraCode.mjs to dump usable code

---
+++ js/constants.mjs
@@ -0,0 +1,2 @@
+export const SUPABASE_BASE_URL = "https://zngenntzebqbjiznfsug.supabase.co/rest/v1";
+export const SUPABASE_FILM_URL = "https://zngenntzebqbjiznfsug.supabase.co/rest/v1/Film Photos";
